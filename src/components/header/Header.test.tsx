8d136620af280a99f266e585621788c374063764
Viktoriia Fedotkina <viktoriia_fedotkina@epam.com>
2022-08-15 17:51:26 +0300

    header tests

--- src/components/header/Header.test.tsx
+++ src/components/header/Header.test.tsx
@@ -1,18 +1,23 @@
 import * as React from "react";
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 
 import { renderWithProviders } from "../../utils/test-utils";
 import { Header } from "./Header";
 import userEvent from "@testing-library/user-event";
 
 describe("renders header", () => {
-    test("opens add movie modal on click 'Add movie' button", () => {
+    test("opens add movie modal on click 'Add movie' button", async () => {
         const user = userEvent.setup();
 
         renderWithProviders(<Header />);
 
-        expect(screen.queryByAltText(/netflix roulette logo/i)).toBeVisible();
+        expect(screen.getByAltText(/netflix roulette logo/i)).toBeVisible();
 
-        user.click(screen.getByRole("button", { name: /Add movie/i }));
+        await user.click(screen.getByRole("button", { name: /\+ Add movie/i }));
+        // expect(await screen.findByText("ADD MOVIE")).toBeVisible();
+
+        await waitFor(() => {
+            expect(screen.getByText("ADD MOVIE")).toBeInTheDocument();
+        });
     });
 });
